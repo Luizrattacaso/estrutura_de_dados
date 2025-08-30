846a44c431c5938b7a36a8846997bc8592c80a21
Luizrattcaso <rattacasoluiz03@gmail.com>
2025-08-30 20:00:59 -0300

    primeiro commit

---
+++ tipos/Fila.java
@@ -0,0 +1,57 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+
+public class Fila {
+    public static void main(String[] args){
+    
+    /*A fila segue a estrutura FIFO(First-In, First-Out)
+    Por exemplo, pode ser utilizada em uma fila de cinema onde o primeiro a chegar é o primeiro a ser atendido
+    
+    Conceitos
+
+    -Head ---> Primeiro elemento da fila
+    -Tail ---> Ultimo elemento da fila
+    -Enqueue ---> Adicionar um elemento no final da fila
+    -Dequeue ---> Remover o primeiro elemento da fila
+
+    Funções
+    
+    - queue.offer(obj) ---> Adiciona um item na fila
+    - queue.peek() ---> Retorna o primeiro item da fila
+    - queue.poll() ---> Remove o primeiro item da fila. Se utilizado com o print você imprime e deleta ao mesmo tempo
+    - queue.isEmpty() ---> Checa se a fila está vazia
+    - queue.size() ---> Length da fila
+
+    */
+
+
+   //exemplo de uso
+
+   Scanner scanner = new Scanner(System.in);
+
+   Queue<String> queue = new LinkedList<String>(); // tem que usar LinkedList porque ela implementa a interface Queue, permitindo operações de fila (FIFO).
+        
+   System.out.println("Bem-vindo ao cinema🍿!");
+        System.out.println("Entre com o nome dos 5 clientes que entraram na fila: ");
+
+        for (int i = 0; i<5 ; i++){
+            String name = scanner.nextLine();
+            queue.offer(name.toUpperCase());
+        }
+
+        System.out.printf("%n");
+        System.out.printf("Clientes na fila: %s", queue);
+
+        while (!queue.isEmpty()){
+            System.out.printf("%nPróximo cliente da fila a ser chamado: %s%n",queue.peek());
+            System.out.printf("Cliente %s, pode entrar no cinema!%n", queue.poll());
+            System.out.printf("Clientes na fila = %d: %s%n", queue.size(), queue);
+            System.out.printf("%n");
+        }
+        
+        scanner.close();
+    }
+}
---
+++ tipos/Pilha.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.util.Stack; //Stack == Pilha
+
+public class Pilha {
+    public static void main(String[] args) {
+
+        /* Seguem uma estrutura LIFO(Last in First out).
+
+        Por exemplo, pode ser utilizadA em histórico de navegação
+        onde o ultimo site será o topo da nossa pilha
+
+        Funções
+        - stack.empty() ---> checa se a pilha esta vazia
+        - stack.push(name) ---> A função push adiciona itens na nossa função
+        - stack.peek() ---> A função peek retorna o topo da pilha
+        - stack.pop() ---> Remove o topo da lista. Se utilizado com o print você imprime e deleta ao mesmo tempo
+        - stack.size() ---> length da pilha
+        - stack.search(name) ---> Retorna a posição do item na pilha. Se o item não existir retorna -1
+        - stack.contains(name) ---> Retorna true ou false se o item existir na pilha
+
+        */
+
+        Scanner scanner = new Scanner(System.in);
+        Stack<String> stack = new Stack<String>(); //cria um objeto do tipo Stack que suporta Strings
+        Stack<String> stack2 = new Stack<String>();
+
+        for (int i = 0; i<=5 ; i++){
+            System.out.print("Enter a web site: ");
+            String name = scanner.nextLine();
+            stack.push(name);
+        }
+
+        String response = "a";
+
+        while (!response.equalsIgnoreCase("q")) {
+            System.out.printf("check your history. Select%n- r to return page%n- n to next page%n- q to quit the history%nResponse: ");
+            response = scanner.nextLine();
+
+            if (response.equalsIgnoreCase("r")){
+                if (stack.isEmpty()){
+                    System.out.println("this is the latest page");
+                }
+                stack2.push(stack.pop());
+                System.out.printf("Actual page: %s", stack.peek());
+                System.out.println("%n");
+            } else if (response.equalsIgnoreCase("n")){
+                if (stack.isEmpty()){
+                    System.out.println("this is the most fresh page");
+                }
+                stack.push(stack2.pop());
+                System.out.printf("Actual page: %s", stack.peek());
+                System.out.println("%n");
+            } else {
+                System.out.println("couldn't understand your response");
+                break;
+            }
+        }
+        scanner.close();
+    }
+}
